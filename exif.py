62894a72776eb1552cd3e5bd318876b666fced1c
kevinkinking <kevin.works@qq.com>
2018-05-29 17:42:58 +0800

    edit code

--- exif.py
+++ exif.py
@@ -3,6 +3,7 @@ from PIL.ExifTags import TAGS
 import io
 import urllib
 import numpy as np
+import cv2
 
 def get_exif_from_bytes(img_bytes):
     img_data = io.BytesIO(img_bytes)
@@ -30,17 +31,32 @@ def get_exif(img):
     return ret
 
 def get_attr_img_from_url(img_url):
-    resp = urllib.urlopen('test.jpg')
-    img_bytes = np.asarray(bytearray(resp.read()), dtype = 'uint8')
-    img_data = io.BytesIO(img_bytes)
-    img = Image.open(img_data)
+    try:
+        resp = urllib.urlopen(img_url)
+        img_bytes = np.asarray(bytearray(resp.read()), dtype = 'uint8')
+        img_data = io.BytesIO(img_bytes)
+        img = Image.open(img_data)
+    except Exception as e:
+        return 401, None, None
+    img_cv = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
     exif = get_exif(img)
-
-    print exif['GPSInfo']
-    print exif['DateTime']
+    res_dic = {}
+    if exif.has_key('GPSInfo'):
+        res_dic['GPSInfo'] = exif['GPSInfo']
+    if exif.has_key('DateTime'):
+        res_dic['DateTime'] = exif['DateTime']
+    return 201, res_dic, img_cv
 
 if __name__ == '__main__':
-    get_attr_img_from_url('test.jpg')
+    code, res_dic, img = get_attr_img_from_url('nono.jpg')
+    print res_dic
+    if code != 201:
+        print code
+    else:
+        print res_dic['GPSInfo']
+        print res_dic['DateTime']
+        cv2.imshow('show', img)
+        cv2.waitKey(0)
     # resp = urllib.urlopen('test.jpg')
     # img_bytes = np.asarray(bytearray(resp.read()), dtype = 'uint8')
     # exif = get_exif_from_bytes(img_bytes)
